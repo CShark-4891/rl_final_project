8238054625ae73b1aaee2521ef80657723afbdd6
Alex Alfonso Trigo <119543558+plex4451@users.noreply.github.com>
2026-08-13 21:22:36 +0200

    Added d4rl dataset evaluation

--- src/configs/default_paths.py
+++ src/configs/default_paths.py
@@ -1,4 +1,4 @@
-DATASET_PROFILES_DIR = "src\\results\\analysis_results\\dataset_profiles"
+DATASET_PROFILES_DIR = "src\\results\\analysis_results\\dataset_profiles_d4rl"
 POLICY_RESULTS_DIR = "src\\results\\policy_results"
 PIPELINE_RESULT_DIR = POLICY_RESULTS_DIR + "\\self_trained_cql_runs"
 DEFAULT_CONFIG_PATH = "src\\configs\\cql_default.yaml"
