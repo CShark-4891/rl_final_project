5c6c7e1b96fd6ff9ea41460a84e663992d95c770
CShark-4891 <152265410+CShark-4891@users.noreply.github.com>
2026-08-15 12:24:56 +0200

    fix bug in ERI computation

--- src/dataset_analyzer/profile_feature_computer.py
+++ src/dataset_analyzer/profile_feature_computer.py
@@ -37,9 +37,9 @@ class ProfileFeatureComputer:
             return 0.0  # Avoid division by zero; all returns are the same
 
         if min_return < 0:
-            return float(((mean_return - min_return) - (max_return - min_return)) / (max_return - min_return))
+            return float(((max_return - min_return) - (mean_return - min_return)) / (mean_return - min_return))
         else:
-            return float(((mean_return) - (max_return)) / (max_return))
+            return float(((max_return) - (mean_return)) / (mean_return))
 
     def compute_mean_estimated_action_stochasticity(actions, states, epochs=50, batch_size=256) -> float:
         """Estimate the Action Stochasticity (EAS) of the behavior policy that
